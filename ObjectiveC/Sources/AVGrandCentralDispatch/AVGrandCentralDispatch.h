c660beb1afedc358699328fb5f55622fcef3268e
Aleksandr Voropaev <aleksandr.voropayev@gmail.com>
2016-08-25 18:54:35 +0300

    GCD works

---
+++ ObjectiveC/Sources/AVGrandCentralDispatch/AVGrandCentralDispatch.h
@@ -0,0 +1,13 @@
+//
+//  AVGrandCentralDispatch.h
+//  ObjectiveC
+//
+//  Created by Aleksandr Voropaev on 8/25/16.
+//  Copyright © 2016 Aleksandr Voropaev. All rights reserved.
+//
+
+#ifndef AVGrandCentralDispatch_h
+#define AVGrandCentralDispatch_h
+
+
+#endif /* AVGrandCentralDispatch_h */
---
+++ ObjectiveC/Sources/AVGrandCentralDispatch/AVGrandCentralDispatch.m
@@ -0,0 +1,9 @@
+//
+//  AVGrandCentralDispatch.m
+//  ObjectiveC
+//
+//  Created by Aleksandr Voropaev on 8/25/16.
+//  Copyright © 2016 Aleksandr Voropaev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
